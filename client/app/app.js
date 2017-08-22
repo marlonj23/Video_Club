c5eacf1f7ddd52f46a75cf251aeafeeb4e6c14dd
MarlonJ23 <marlon_comercio28@hotmail.com>
2017-08-22 04:50:49 -0500

    se creo videoClub

---
+++ client/app/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('videoClubApp', [
+        'videoClubApp.constants',
+        'ngCookies',
+        'ngResource',
+        'ngMessages',
+        'ngSanitize',
+        'ui.router',
+        'ngFileUpload',
+        'satellizer',
+        'ngImgCrop',
+        'ngFileSaver',
+        'LocalStorageModule'
+    ])
+    .constant("API", "http://localhost:8080/adsi2017_1199561-master")
+    .config(function($urlRouterProvider, $locationProvider, $authProvider, API,localStorageServiceProvider) {
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode(true);
+        // Config Satellizer
+        $authProvider.loginUrl = API + '/api/auth/login';
+        $authProvider.tokenName = 'token';
+        $authProvider.tokenPrefix = 'VideoClubAdsi';
+
+        localStorageServiceProvider
+            .setPrefix('VideoClubAdsi');
+    });
